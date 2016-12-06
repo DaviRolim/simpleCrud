9fe590e948e2b6287af27f547a6825e0de34a1f5
Davi Rolim <davi.rolim@hotmail.com>
2016-12-06 00:28:06 -0300

    SpringConfiguration file almost ready

--- src/main/java/br/com/crud/conf/SpringConfig.java
+++ src/main/java/br/com/crud/conf/SpringConfig.java
@@ -1,5 +1,47 @@
 package br.com.crud.conf;
 
-public class SpringConfig {
+import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[0];
+		}
+
+	/**
+	 * Metodo onde coloco as classes que o spring vai procurar
+	 * para fazer a configuração
+	 * como a configuração base AppConfiguration
+	 * e configurações como JPA
+	 */
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+//		return new Class[] {};
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		
+		return new Filter[] {characterEncodingFilter};
+	}
+	
+	 @Override
+	    protected void customizeRegistration(Dynamic registration) {
+	    	registration.setMultipartConfig(new MultipartConfigElement(""));
+	    }
 
 }
